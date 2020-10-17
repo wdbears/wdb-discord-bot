364243f436c485fd06dcebb1570f34f882d1ab9f
Ahmad Raza <razatastic@gmail.com>
2020-10-16 21:13:13 -0400

    Allow semicolons in Prettier config

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
-import dotenv from 'dotenv'
-import Discord from 'discord.js'
-import ytdl from 'ytdl-core'
+import dotenv from 'dotenv';
+import Discord from 'discord.js';
+import ytdl from 'ytdl-core';
 
-dotenv.config() // Load instance variables
+dotenv.config(); // Load instance variables
 
 // Create an instance of a Discord client
-const client = new Discord.Client()
+const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log('Nom Nom is ready to munch, mmm~')
-})
+  console.log('Nom Nom is ready to munch, mmm~');
+});
 
 client.on('message', async (message) => {
   if (message.channel.name === 'bot-commands') {
-    const messageIgnoreCase = message.content.toUpperCase()
+    const messageIgnoreCase = message.content.toUpperCase();
 
     switch (messageIgnoreCase) {
       case 'MONKE': {
-        const connection = await message.member.voice.channel.join()
+        const connection = await message.member.voice.channel.join();
         const dispatcher = connection.play(
           ytdl('https://www.youtube.com/watch?v=Xz1iSC-UFtk', {
             filter: 'audioonly',
           })
-        )
+        );
         // End of the stream
         dispatcher.on('finish', () => {
-          console.log('Finished playing!')
-          dispatcher.destroy()
-          message.member.voice.channel.leave()
-        })
-        break
+          console.log('Finished playing!');
+          dispatcher.destroy();
+          message.member.voice.channel.leave();
+        });
+        break;
       }
 
       default:
-        break
+        break;
     }
   }
-})
+});
 
-client.login(process.env.DISCORD_CLIENT_ID)
+client.login(process.env.DISCORD_CLIENT_ID);
