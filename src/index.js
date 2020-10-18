4b0e6f0e08bee2bcb6ceb8808ad9a214c9d002a5
Ahmad Raza <razatastic@gmail.com>
2020-10-17 23:53:13 -0400

    Add prefix, argument handler

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
 import dotenv from 'dotenv';
 import Discord from 'discord.js';
-import ytdl from 'ytdl-core';
+// import ytdl from 'ytdl-core';
 
 dotenv.config(); // Load instance variables
+
+const prefix = '!'; // Prefix for bot trigger command
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.once('ready', () => {
   console.log('Nom Nom is ready to munch, mmm~');
 });
 
 client.on('message', async (message) => {
-  if (message.channel.name === 'bot-commands') {
-    const messageIgnoreCase = message.content.toUpperCase();
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
 
-    switch (messageIgnoreCase) {
-      case 'MONKE': {
-        const connection = await message.member.voice.channel.join();
-        const dispatcher = connection.play(
-          ytdl('https://www.youtube.com/watch?v=Xz1iSC-UFtk', {
-            filter: 'audioonly',
-          })
-        );
-        // End of the stream
-        dispatcher.on('finish', () => {
-          console.log('Finished playing!');
-          dispatcher.destroy();
-          message.member.voice.channel.leave();
-        });
-        break;
-      }
-
-      default:
-        break;
+  if (command === 'args-info') {
+    if (!args.length) {
+      return message.channel.send(
+        `You didn't provide any arguments, ${message.author}!`
+      );
     }
+    if (args[0] === 'foo') {
+      return message.channel.send('bar');
+    }
+    message.channel.send(`First argument: ${args[0]}`);
+  } else if (command === 'kick') {
+    if (!message.mentions.users.size) {
+      return message.reply('you need to tag a user in order to kick them!');
+    }
+
+    const taggedUser = message.mentions.users.first();
+    message.channel.send(`You wanted to kick: ${taggedUser.username}`);
   }
 });
 
