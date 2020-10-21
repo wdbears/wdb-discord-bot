874662ae70bbffac2234123fbb36a1f7afc7ea67
Ahmad Raza <razatastic@gmail.com>
2020-10-21 11:07:03 -0400

    Clean up comments in index.js

--- src/index.js
+++ src/index.js
@@ -10,26 +10,22 @@ admin.initializeApp({
   credential: admin.credential.applicationDefault(),
   databaseURL: 'https://wdb-discord-bot.firebaseio.com',
 });
-
 const db = admin.database();
-
 const ref = db.ref();
 
+// Cache commands from firebase
 const commandKeywords = new Map();
-
 ref.on(
   'value',
   (snapshot) => {
     snapshot.forEach((collection) => {
-      // const { key } = collection;
       collection.forEach((val) => {
-        // console.log(val);
         commandKeywords.set(val.key, val.val());
       });
     });
   },
   (errorObject) => {
-    console.log(`The read failed: ${errorObject.code}`);
+    console.error(`The read failed: ${errorObject.code}`);
   }
 );
 
