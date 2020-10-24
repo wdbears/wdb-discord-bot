3f165429335f6d546bf1c39c725a5fad9ee8bf4c
Ahmad Raza <razatastic@gmail.com>
2020-10-24 17:44:12 -0400

    Add check to see if user is in server when using command

--- src/server.js
+++ src/server.js
@@ -41,6 +41,10 @@ client.on('message', async (message) => {
 
   const command = client.commands.get(commandName);
 
+  if (command.guildOnly && message.channel.type === 'dm') {
+    return message.reply("I can't execute that command inside DMs!");
+  }
+
   if (command.args && !args.length) {
     let reply = `You didn't provide any arguments, ${message.author}`;
 
