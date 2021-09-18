3c8b1a5aab09dc76d14990955fa84d4726326432
Ahmad Raza <razatastic@gmail.com>
2021-09-18 12:47:28 -0400

    Remove eslint warning

--- src/commands/cryptocurrency/eth.js
+++ src/commands/cryptocurrency/eth.js
@@ -3,6 +3,7 @@ import { execute as getCrypto } from './crypto';
 export const name = 'eth';
 export const description = 'Fetch the current price of Ethereum.';
 
+// eslint-disable-next-line no-unused-vars
 export async function execute(message, args) {
   getCrypto(message, 'eth');
 }
