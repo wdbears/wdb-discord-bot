e5b709e3805db94fc8f45d833b710ca0e94e8b7c
Ahmad Raza <razatastic@gmail.com>
2021-11-20 01:34:16 -0500

    Remove CustomInteraction

--- src/models/CustomInteraction.ts
+++
@@ -1,8 +0,0 @@
-// import { CommandInteraction } from 'discord.js';
-// import CustomClient from './CustomClient';
-
-// class CustomInteraction extends CommandInteraction {
-//   constructor() {
-//     super();
-//   }
-// }
