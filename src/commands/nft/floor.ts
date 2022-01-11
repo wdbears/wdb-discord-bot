349d303684967f5c7ada3d278bef576d5ec6e10e
Ahmad Raza <razatastic@gmail.com>
2022-01-11 00:13:01 -0500

    Move choices to array

--- src/commands/nft/floor.ts
+++ src/commands/nft/floor.ts
@@ -9,6 +9,29 @@ export const getFloorPrice = async (collection: string) => {
   return res.stats.floor_price;
 };
 
+const choices: [name: string, value: string][] = [];
+choices.push(['snails', 'evosnails-nft']);
+choices.push(['pixels', 'the-pixels-inc']);
+choices.push(['bearx', 'bearxlabs']);
+choices.push(['noundles', 'noundles']);
+choices.push(['kaiju', 'kaiju-kingz']);
+choices.push(['galacticapes', 'galacticapes']);
+choices.push(['MAYC', 'mutant-ape-yacht-club']);
+choices.push(['cyberkongz', 'cyberkongz']);
+choices.push(['pudgypenguins', 'pudgypenguins']);
+choices.push(['coolcats', 'cool-cats-nft']);
+choices.push(['sherbet', 'playsherbet']);
+choices.push(['littles', 'thelittlesnft']);
+choices.push(['uncoolcats', 'uncool-cats-nft']);
+choices.push(['clonex', 'clonex-mintvial']);
+choices.push(['webb', 'worldwidewebbland']);
+choices.push(['cryptofoxes', 'cryptofoxes-v2']);
+choices.push(['metazoo', 'metazoo-games-tokens']);
+choices.push(['syncity', 'syn-city-passes']);
+choices.push(['wnd', 'wizards-dragons-game-v2']);
+choices.push(['outeridentities', 'neotokyo-outer-identities']);
+choices.push(['thingdoms', 'thingdoms-nft-official']);
+
 const floor: ICommand = {
   name: 'floor',
   description: 'Get floor price',
@@ -17,27 +40,7 @@ const floor: ICommand = {
       .setName('collection')
       .setDescription('the nft collection for which the floor is being retrieved')
       .setRequired(true)
-      .addChoice('snails', 'evosnails-nft')
-      .addChoice('pixels', 'the-pixels-inc')
-      .addChoice('bearx', 'bearxlabs')
-      .addChoice('noundles', 'noundles')
-      .addChoice('kaiju', 'kaiju-kingz')
-      .addChoice('galacticapes', 'galacticapes')
-      .addChoice('MAYC', 'mutant-ape-yacht-club')
-      .addChoice('cyberkongz', 'cyberkongz')
-      .addChoice('pudgypenguins', 'pudgypenguins')
-      .addChoice('coolcats', 'cool-cats-nft')
-      .addChoice('sherbet', 'playsherbet')
-      .addChoice('littles', 'thelittlesnft')
-      .addChoice('uncoolcats', 'uncool-cats-nft')
-      .addChoice('clonex', 'clonex-mintvial')
-      .addChoice('webb', 'worldwidewebbland')
-      .addChoice('cryptofoxes', 'cryptofoxes-v2')
-      .addChoice('metazoo', 'metazoo-games-tokens')
-      .addChoice('syncity', 'syn-city-passes')
-      .addChoice('wnd', 'wizards-dragons-game-v2')
-      .addChoice('outeridentities', 'neotokyo-outer-identities')
-      .addChoice('thingdoms', 'thingdoms-nft-official')
+      .addChoices(choices)
   ),
   execute: async (interaction: CommandInteraction): Promise<void> => {
     const collection: string = interaction.options.getString('collection')!;
