a555a39313c18d788da14b67de92829a05d9a336
Ahmad Raza <razatastic@gmail.com>
2022-01-12 12:54:59 -0500

    Add logging on successful deploy

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,12 +1,20 @@
+import { TextChannel } from 'discord.js';
 import { client } from '../config/bot';
 import { getGasPrices } from '../helpers/etherscan';
 import IEvent from '../models/IEvent';
+
+const isProd = process.env['NODE_ENV'] === 'production';
 
 const ready: IEvent = {
   name: 'ready',
   once: false,
   execute: (): void => {
     console.log('Ready!');
+
+    if (isProd) {
+      const channel = <TextChannel>client.channels.cache.get('logs');
+      if (channel) channel.send(client.user?.username + ' has successfully deployed!');
+    }
 
     setInterval(async () => {
       const gasPrices = await getGasPrices();
