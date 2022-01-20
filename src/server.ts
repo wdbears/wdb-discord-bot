23d4eefb361ed86eb3d10d7a467de1991d5dba97
Luis Olivar <luis.olivar@nyu.edu>
2022-01-19 20:16:45 -0500

    Export prod check

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import { initBot } from './config/bot';
 // import path from 'path';
 
-const isProd = process.env['NODE_ENV'] === 'production';
+export const isProd = process.env['NODE_ENV'] === 'production';
 const BOT_TOKEN = isProd ? process.env['BOT_TOKEN']! : process.env['BOT_TOKEN_TEST']!;
 
 // Setup listener on port 8080 (required for cloud deploys)
